5a8cda339ebc2dc4e1cff86ea356cc5644d049ea
helgakern <helgakern@gmail.com>
2020-04-23 14:40:53 -0700

    created actions create and destroy for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,26 @@
 class ReviewsController < ApplicationController
+    def create
+        @product = Product.find(params[:product_id])
+        @review = Review.new review_params
+        @review.product = @product
+        if @review.save
+            redirect_to product_path(@product)
+        else
+            @reviews = product.reviews.order(created_at: :desc)
+            render 'products/show'
+        end
+    end
+
+    def destroy
+    @review = Review.find params[:id]
+    @review.destroy
+    redirect_to product_path(@review.product)
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:body)
+    end
+
 end
