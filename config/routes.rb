cbc2e26c2b3b83720cd246f679714caa53b4e501
helgakern <helgakern@gmail.com>
2020-04-23 13:59:40 -0700

    added resources in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :products
+  resources :products do
+    resources :reviews, only: [:create, :destroy]
+  end
 
   get("/", to: "welcome#root")
   get("/about", to: "welcome#about")
