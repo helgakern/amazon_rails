4fc9f1cc9b77acb399e95c5d8ca9c6e1624a76cc
helgakern <helgakern@gmail.com>
2020-04-27 14:35:35 -0700

    generated user model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,10 +1,5 @@
 class Review < ApplicationRecord
+    validates :rating, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  
     belongs_to :product
-
-    validates :rating, {
-        numericality: {
-            greater_than_or_equal_to: 1,
-            less_than_or_equal_to: 5
-        }
-    }
-end
+  end
