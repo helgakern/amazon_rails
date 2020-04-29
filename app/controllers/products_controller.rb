2076297354ef9091cab3c7a0bad9620c3762b652
helgakern <helgakern@gmail.com>
2020-04-28 17:43:40 -0700

    updated products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,13 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :load_product!, except: [:create]
+  before_action :authorize_user!, only: [:edit, :update, :destroy]
 
   def index
     @products = Product.all.order('created_at DESC')
   end
 
   def new
-    @product = Product.new
   end
 
   def create
@@ -20,22 +21,18 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
     @review = Review.new
   end
 
   def destroy
-    @product = Product.find(params[:id])
     @product.destroy
     redirect_to products_path
   end
 
   def edit
-    @product = Product.find(params[:id])
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update product_params
       redirect_to product_path @product
     else
@@ -50,4 +47,20 @@ class ProductsController < ApplicationController
     # docs about .permit() https://api.rubyonrails.org/classes/ActionController/Parameters.html#method-i-permit
     params.require(:product).permit(:title, :description, :price, :sale_price)
   end
+
+  def authorize_user!
+    unless can? :crud, @product
+      flash[:danger] = "Access Denied"
+      redirect_to root_path
+    end
+  end
+
+  def load_product!
+    if params[:id].present?
+      @product = Product.find(params[:id])
+    else
+      @product = Product.new
+    end
+  end
 end
+
